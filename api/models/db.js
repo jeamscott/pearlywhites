a9cdf80f605867c90bd988c31eb8d9148cbdaaf1
Joseph Jones <josephjones@Josephs-MacBook-Pro.local>
2018-03-12 22:48:47 -0400

    use newer connection method for Mongoose

--- api/models/db.js
+++ api/models/db.js
@@ -1,11 +1,21 @@
 var mongoose = require('mongoose');
 var gracefulShutdown;
-var dbURI = 'mongodb://localhost/meanAuth';
+var dbURI = 'mongodb://localhost/pearlyWhites';
 if (process.env.NODE_ENV === 'production') {
   dbURI = process.env.MONGOLAB_URI;
 }
 
-mongoose.connect(dbURI);
+const options = {
+  useMongoClient: true,
+  autoIndex: false, // Don't build indexes
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
+};
+
+mongoose.connect(dbURI, options);
 
 // CONNECTION EVENTS
 mongoose.connection.on('connected', function() {
