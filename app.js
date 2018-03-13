80d7b57527af03337f4ce3dd8b4eabc10125e3ec
Joseph Jones <josephjones@Josephs-MacBook-Pro.local>
2018-03-12 22:56:36 -0400

    add line to consume public when deploying

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(express.static('public'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -55,10 +56,10 @@ app.use(function(req, res, next) {
 
 // [SH] Catch unauthorised errors
 app.use(function (err, req, res, next) {
-  if (err.name === 'UnauthorizedError') {
-    res.status(401);
-    res.json({"message" : err.name + ": " + err.message});
-  }
+    if (err.name === 'UnauthorizedError') {
+        res.status(401);
+        res.json({"message" : err.name + ": " + err.message});
+    }
 });
 
 // development error handler
