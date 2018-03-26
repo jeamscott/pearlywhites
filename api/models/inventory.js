cf760f8bff5bbc18d7d3cb7b769152192c7e6d70
Lori Wittman <lwittman1@student.cscc.edu>
2018-03-26 19:47:49 -0400

    db update
    db update

---
+++ api/models/inventory.js
@@ -0,0 +1,26 @@
+var mongoose = require( 'mongoose' );
+
+var inventorySchema = new mongoose.Schema({
+    id_number: {
+        type: String,
+        required: true
+      },
+      item_name: {
+        type: String,
+        required: true
+      },
+    product_description: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: String,
+        required: true
+    },
+    cost:{
+        type: String,
+        required: true
+    }
+      
+    });
+    mongoose.model('Inventory', inventorySchema);
