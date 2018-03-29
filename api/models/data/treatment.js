59ac81caa70f6100e7e50181c8ab0c0cf743e0a6
Lori Wittman <lwittman1@student.cscc.edu>
2018-03-29 19:08:41 -0400

    update file in treatment table

--- api/models/data/treatment.js
+++ api/models/data/treatment.js
@@ -1,6 +1,6 @@
 exports.data = function() {
     return [
-        {id: "1", treatment_id: "D0210", description: "Full mouth Xrays ", tooth_id: "NA"}
-        
+        {id: "1", treatment_id: "D0210", description: "Full mouth Xrays ", tooth_id: "NA"},
+        {id: "1", treatment_id: "D0220", description: "Periapical ", tooth_id: "NA"}
     ];
 } 
