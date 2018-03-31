8290dfbe6d3ab358c1d5b46584cddd3bd9ca522c
Lori Wittman <lwittman1@student.cscc.edu>
2018-03-31 18:56:37 -0400

    user account added to db

---
+++ api/models/data/user_account.js
@@ -0,0 +1,6 @@
+exports.data = function() {
+    return [
+        {id_number:"1", user_name:" ", user_email:"", user_password:""},
+        {id_number:"2", user_name:" ", user_email:"", user_password:""}
+    ];
+}
