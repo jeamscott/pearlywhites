1145089484002e4c565f2673491e4ab8d66fcec6
Lori Wittman <lwittman1@student.cscc.edu>
2018-03-31 19:42:25 -0400

    update user account db

--- api/models/data/role.js
+++ api/models/data/role.js
@@ -1,6 +1,8 @@
 exports.data = function() {
     return [
-        {id_number:"1", role_name:" "},
-        {id_number:"2", role_name:" "}
+        {id_number:"1", role_name:"dentist"},
+        {id_number:"2", role_name:"hygienist"},
+        {id_number:"3", role_name:"assistant"},
+        {id_number:"4", role_name:"staff"}
     ];
 } 
--- api/models/data/user_account.js
+++ api/models/data/user_account.js
@@ -1,6 +1,6 @@
 exports.data = function() {
     return [
-        {id_number:"1", user_name:" ", user_email:"", user_password:""},
+        {id_number:"1", user_name:"mreynolds", user_email:"mreynolds@pearlywhites.com", user_password:"mreynolds"},
         {id_number:"2", user_name:" ", user_email:"", user_password:""}
     ];
 }
