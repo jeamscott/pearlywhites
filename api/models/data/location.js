1a9d1dbb29a58474435bc6bee21febc10916da53
Lori Wittman <lwittman1@student.cscc.edu>
2018-04-02 18:02:55 -0400

    city update in location db

--- api/models/data/location.js
+++ api/models/data/location.js
@@ -1,6 +1,6 @@
 exports.data = function() {
     return [
-        {id_number: "1", phone_number: "754-5551212", street: " ", city: " ", state: " ", zip: " "},
-        {id_number: "2", phone_number: "305-555-1212", street: " ", city: " ", state: " ", zip: " "}
+        {id_number: "1", phone_number: "754-5551212", street: " ", city: "Ft. Lauderdale", state: "Fl", zip: "33301"},
+        {id_number: "2", phone_number: "305-555-1212", street: " ", city: "Miami", state: "Fl", zip: "33142"}
     ];
 } 
