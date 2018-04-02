9c34cb5649deb172c948fbd678c943903b1354ff
Lori Wittman <lwittman1@student.cscc.edu>
2018-04-02 18:27:13 -0400

    patient collection update

--- api/models/data/patients.js
+++ api/models/data/patients.js
@@ -1,7 +1,7 @@
 exports.data = function() {
     return [
-        {id_number:"1", first_name:"Tom", last_name:"Jones", id_number:" ", phone_number:" ",street:" ", city: " ", state: " ", zip_code: " ", email_address: " "},
-        {id_number:"2", first_name:"Joe", last_name:"Johnson", id_number:" ", phone_number:" ",street:" ", city: " ", state: " ", zip_code: " ", email_address: " "},
-        {id_number:"3", first_name:"John", last_name:"Doe", id_number:" ", phone_number:" ",street:" ", city: " ", state: " ", zip_code: "12345", email_address: "jdoe@example.com"}
+        {id_number:"1", first_name:"Tom", last_name:"Jones", id_number:"261-12-1212", phone_number:"754-555-2233",street:"12 Brooks St.", city: "Ft. Lauderdale", state: "Fl", zip_code: "33301", email_address: "tjones@gmail.com"},
+        {id_number:"2", first_name:"Joe", last_name:"Johnson", id_number:"267-12-1213 ", phone_number:"305-555-1212",street:"444 Wagon Rd.", city: "Miami", state: "Fl", zip_code: "33142", email_address: "jjohnson@gmail.com"},
+        {id_number:"3", first_name:"John", last_name:"Doe", id_number:"589-12-1214", phone_number:"321-555-1255",street: "12333 Hayday Dr.", city: "Orlando", state: "Fl", zip_code: "32801", email_address: "jdoe@gmail.com"}
     ];
 } 
