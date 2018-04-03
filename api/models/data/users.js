48845ad88745e24e3661c20aab3bd94dcdaec56a
Lori Wittman <lwittman1@student.cscc.edu>
2018-04-02 21:18:33 -0400

    email address added

--- api/models/data/users.js
+++ api/models/data/users.js
@@ -1,6 +1,6 @@
 exports.data = function() {
     return [
-        {id_number: "1", email: " ", name: " "},
-        {id_number: "2", email: " ", name: " "}
+        {id_number: "1", email: "moeller@test.com ", name: " "},
+        {id_number: "2", email: "enamel@test.com ", name: " "}
     ];
 } 
