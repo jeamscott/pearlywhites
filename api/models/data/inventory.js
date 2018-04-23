312fb6fa8c440887d21f7c47dbbc00be02e6e656
Lori Wittman <lwittman1@student.cscc.edu>
2018-04-22 21:19:49 -0400

    database inventory update

--- api/models/data/inventory.js
+++ api/models/data/inventory.js
@@ -1,6 +1,12 @@
 exports.data = function() {
     return [
         {id_number: "1", item_name: "Gum", product_description: "Tooth brush", quantity: "1,000", cost: "1.00 ea"},
-        {id_number: "2", item_name: "Prophy Paste", product_description: "Polishing paste", quantity: "1,000", cost: "1.00 ea"}
+        {id_number: "2", item_name: "Prophy Paste", product_description: "Prophy Paste", quantity: "1,000", cost: "1.00 ea"},
+        {id_number: "3", item_name: "Floss", product_description: "Floss", quantity: "1,000", cost: "1.00 ea"},
+        {id_number: "4", item_name: "Tooth Paste", product_description: "Tooth Paste", quantity: "1,000", cost: "1.00 ea"},
+        {id_number: "5", item_name: "Mouth Wash", product_description: "Mouth Wash", quantity: "1,000", cost: "1.00 ea"},
+        {id_number: "6", item_name: "Prophy Paste", product_description: "Polishing paste", quantity: "1,000", cost: "1.00 ea"}
+   
+
     ];
 } 
