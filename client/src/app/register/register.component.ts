afc0a528ae9aa5c444a79146f8c2134ea167b607
Jeffery Scott <jeamscott@gmail.com>
2018-05-01 21:12:52 -0400

    register fixed

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -15,7 +15,7 @@ export class RegisterComponent {
   constructor(private auth: AuthenticationService, private router: Router) {}
 
   register() {
-    this.auth.register(this.credentials).subscribe(() => {
+    this.auth.nsiregister(this.credentials).subscribe(() => {
       this.router.navigateByUrl('/edit.profile');
     }, (err) => {
       console.error(err);
