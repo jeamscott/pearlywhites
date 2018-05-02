2d8eee8760ef7bf7ac099d7d85ade351f5d785d9
Jeffery Scott <jeamscott@gmail.com>
2018-05-01 21:16:28 -0400

    home fixed

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   templateUrl: './home.component.html'
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  constructor(public auth: AuthenticationService) {}
+
+}
