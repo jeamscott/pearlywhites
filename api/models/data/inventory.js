0de02948e455aaa7a2d7f35c582b4cc2422c90e5
Lori Wittman <lwittman1@student.cscc.edu>
2018-05-03 18:53:20 -0400

    updating inventories db

--- api/models/data/inventory.js
+++ api/models/data/inventory.js
@@ -1,13 +1,24 @@
 exports.data = function() {
     return [
-        {id_number: "1", item_name: "Monitor", product_description: "Dell", quantity: "400", cost: "200.00 ea"},
-        {id_number: "2", item_name: "Prophy Paste", product_description: "Prophy Paste", quantity: "1,000", cost: "1.00 ea"},
-        {id_number: "3", item_name: "Floss", product_description: "Floss", quantity: "1,000", cost: "1.00 ea"},
-        {id_number: "4", item_name: "Tooth Paste", product_description: "Tooth Paste", quantity: "1,000", cost: "1.00 ea"},
-        {id_number: "5", item_name: "Mouth Wash", product_description: "Mouth Wash", quantity: "1,000", cost: "1.00 ea"},
-        {id_number: "6", item_name: "Cavi Wipes", product_description: "Cavi Wipes", quantity: "1,000", cost: "1.00 ea"},
-        {id_number: "7", item_name: "Patient Bibs", product_description: "Patient Bibs", quantity: "1,000", cost: "1.00 ea"}
-   
+        {item_name: "Tooth Brush", product_description: "Gum:", quantity: "Qty.- 1,000", cost: "1.00 ea"},
+        {item_name: "Prophy Paste", product_description: "Prophy Paste:", quantity: "Qty.- 1,000", cost: "1.00 ea"},
+        {item_name: "Floss", product_description: "Glide:", quantity: "Qty.- 1,000", cost: "1.00 ea"},
+        {item_name: "Tooth Paste", product_description: "Crest:", quantity: "Qty.- 1,000", cost: "1.00 ea"},
+        {item_name: "Mouth Wash", product_description: "Listerine:", quantity: "Qty.- 1,000", cost: "1.00 ea"},
+        {item_name: "Cavi Wipes", product_description: "Cavi Wipes:", quantity: "Qty.- 1,000", cost: "1.00 ea"},
+        {item_name: "Patient Bibs", product_description: "Patient Bibs:", quantity: "Qty.- 1,000", cost: "1.00 ea"},
+        {item_name:"Soap", product_description: "Dial:", quantity: "Qty.- 100", cost: "1.00 ea"},
+        {item_name: "Disposable Gowns", product_description: "Disposable Gowns:", quantity: "Qty.- 100", cost: "1.00 ea"},
+        {item_name: "Matrix Band", product_description: "Matrix Band:", quantity: "Qty.- 100", cost: "1.00 ea"},
+        {item_name: "Toffelmire Retainer", product_description: "Toffelmire Retainer:", quantity: "Qty.- 100", cost: "1.00 ea"},
+        {item_name: "Paper Towels", product_description: "Bounty:", quantity: "Qty.- 100", cost: "1.00 ea"},
+        {item_name: "Tissue Paper", product_description: "Kleenex:", quantity: "Qty.- 100", cost: "1.00 ea"},
+        {item_name: "Toilet Paper", product_description: "Charmin:", quantity: "Qty.- 100", cost: "1.00 ea"},
+        {item_name: "Prophy Angle", product_description: "Prophy Angle:", quantity: "Qty.- 100", cost: "1.00 ea"},        
+        {item_name: "Sterilization Pouches", product_description: "Sterilization Pouches:",  quantity: "Qty.- 100", cost: "1.00 ea"},
+        {item_name: "Suction Tips", product_description: "Suction Tips:", quantity: "Qty.- 100", cost: "1.00 ea"},
+        {item_name: "Monitor", product_description: "Dell:", quantity: "Qty.- 400", cost: "200.00 ea"},
+            
 
     ];
 } 
